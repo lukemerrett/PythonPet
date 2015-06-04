e4f37380a43ea6d0073a11b1968ba970f19a904d
Luke Merrett <lukeamerrett@gmail.com>
2015-06-04 21:32:53 +0100

    Moved date related code to datehelper for reuse

---
+++ Helpers/datehelper.py
@@ -0,0 +1,19 @@
+__author__ = 'Luke Merrett'
+
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+
+def add_seconds_to_date(date, seconds):
+    return date + timedelta(seconds=seconds)
+
+def is_date_earlier_than_today(date):
+    return date <= todays_date()
+
+def get_difference_as_relative_delta(latest_date, earliest_date):
+    return relativedelta(latest_date, earliest_date)
+
+def get_total_seconds_difference(latest_date, earliest_date):
+    return (latest_date - earliest_date).total_seconds()
+
+def todays_date():
+        return datetime.now()
--- PetObjects/age.py
+++ PetObjects/age.py
@@ -1,8 +1,7 @@
 __author__ = 'Luke Merrett'
 
-from datetime import datetime, timedelta
 from random import randint
-from dateutil.relativedelta import relativedelta
+from Helpers import datehelper
 
 class Age:
     __minimum_potential_lifespan_in_seconds = 86400  # 1 day
@@ -11,7 +10,7 @@ class Age:
     __lifespan_in_seconds = None
 
     def __init__(self):
-        self.__birth_date = self.__todays_date()
+        self.__birth_date = datehelper.todays_date()
 
         self.__lifespan_in_seconds = randint(
             self.__minimum_potential_lifespan_in_seconds,
@@ -22,14 +21,14 @@ class Age:
         Gets the current age of the pet in seconds
         :return: The total age of the pet in seconds
         """
-        return (self.__todays_date() - self.__birth_date).total_seconds()
+        return datehelper.get_total_seconds_difference(datehelper.todays_date(),self.__birth_date)
 
     def current_age_string(self):
         """
         Gets the age of the pet in a human readable string
         :return: A human readable form of the pets age
         """
-        current_age = relativedelta(self.__todays_date(), self.__birth_date)
+        current_age = datehelper.get_difference_as_relative_delta(datehelper.todays_date(), self.__birth_date)
         return "Your pet is currently %d years %d months %d days %d hours %d minutes old" % (
             current_age.years,
             current_age.months,
@@ -42,9 +41,5 @@ class Age:
         Returns a value indicating whether the pet has reached it's current lifespan
         :return: True if the pet is dead, false if the pet is still kicking around.
         """
-        time_of_death = self.__birth_date + timedelta(seconds=self.__lifespan_in_seconds)
-        return time_of_death <= self.__todays_date()
-
-    @staticmethod
-    def __todays_date():
-        return datetime.now()
+        time_of_death = datehelper.add_seconds_to_date(self.__birth_date, self.__lifespan_in_seconds)
+        return datehelper.is_date_earlier_than_today(time_of_death)
