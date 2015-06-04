d887e3e889fbbfa79ef687f80d7b20ef63dcf9bd
Luke Merrett <lukeamerrett@gmail.com>
2015-06-04 20:44:40 +0100

    Allowed the pet to age

--- pet.py
+++ pet.py
@@ -1,5 +1,30 @@
 __author__ = 'Luke Merrett'
 
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
 class Pet:
+    birthDate = None
+
     def __init__(self):
         pass
+
+    def hatch(self):
+        self.birthDate = self.__todays_date()
+        pass
+
+    def current_age(self):
+        return (self.__todays_date() - self.birthDate).total_seconds()
+
+    def current_age_string(self):
+        current_age = relativedelta(self.__todays_date(), self.birthDate)
+        return "Your pet is currently %d years %d months %d days %d hours %d minutes old" % (
+            current_age.years,
+            current_age.months,
+            current_age.days,
+            current_age.hours,
+            current_age.minutes)
+
+    @staticmethod
+    def __todays_date():
+        return datetime.now()
--- pythonpet.py
+++ pythonpet.py
@@ -6,3 +6,6 @@ if __name__ == '__main__':
     print('Creating your pet')
 
     myPet = Pet()
+    myPet.hatch()
+
+    print(myPet.current_age_string())
